08c51b441570054e7cc230f1812a84da8c30dfcb
ubikOne <b.blazejewski@gmail.com>
2017-06-04 21:48:27 +0200

    alpha ver 0.0.2

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const fs          = require('fs');
-const cptable     = require('codepage');
-const makeArray   = require('./modules/makeArray');
-const whiteSpace  = require('./modules/whiteSpace');
-const dest        = require('./modules/destination');
-const slicer      = require('./modules/slicer');
-const chceck      = require('./modules/check');
-const spacer      = require('./modules/spacer');
 const exportData  = require('./scripts/exportData');
 const dbfExtract  = require('./scripts/bdfExtract')
 
